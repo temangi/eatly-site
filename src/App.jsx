d687a0b4a4258073ee25c4cfac25dbc0a4edbcdb
temmirlan <temmirlan@MacBook-Pro--temanbot.local>
2025-05-06 15:04:53 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HomePage from './pages/HomePage'
+import {BrowserRouter , Routes , Route} from "react-router-dom"
+
+function App() {
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+    </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
